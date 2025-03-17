8f712d836e621897675fb4fdb3c227a3e7b96507
babyshout <lsms0523@gmail.com>
2025-03-17 11:05:09 +0900

    spring-security-samples 따라치기 servlet/spring-boot/java/jwt/login/
    gradle 작성중
    
    libs.versions.toml 미작동중, 확인 필요
    
    https://github.com/spring-projects/spring-security-samples/blob/main/servlet/java-configuration/hello-security

---
+++ servlet/java-configuration/hello-security-java-configuration/src/main/java/ga/agency/hellosecurityjavaconfiguration/HelloSecurityJavaConfigurationApplication.java
@@ -0,0 +1,13 @@
+package ga.agency.hellosecurityjavaconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSecurityJavaConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSecurityJavaConfigurationApplication.class, args);
+	}
+
+}
---
+++ servlet/java-configuration/hello-security-java-configuration/src/test/java/ga/agency/hellosecurityjavaconfiguration/HelloSecurityJavaConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.agency.hellosecurityjavaconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSecurityJavaConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
