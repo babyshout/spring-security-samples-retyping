c80b3ebd7fbf5df34cdb17bda9d773af67f30709
babyshout <lsms0523@gmail.com>
2025-03-20 09:34:19 +0900

    spring-security-samples 따라치기
    https://github.com/spring-projects/spring-security-samples/tree/main/servlet/java-configuration/hello-security
    
    코드 따라치는중.. 작동확인 필요
    
    https://github.com/spring-projects/spring-security-samples/blob/main/servlet/java-configuration/hello-security

---
+++ servlet/java-configuration/hello-security-java-configuration/src/main/java/ga/agency/hellosecurityjavaconfiguration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package ga.agency.hellosecurityjavaconfiguration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    public SecurityWebApplicationInitializer() {
+        super(SecurityConfiguration.class);
+    }
+}
